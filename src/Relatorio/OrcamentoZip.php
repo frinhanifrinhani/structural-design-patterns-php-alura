07698be4c28601df6a561907c4322737245003f3
Thiago Frinhani <thiago.frinhani@sindifisconacional.org.br>
2022-03-26 12:10:23 -0300

    Classes de relatorio

---
+++ src/Relatorio/OrcamentoZip.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Alura\DesignPattern\Relatorio;
+
+
+use Alura\DesignPattern\Orcamento;
+
+class OrcamentoZip
+{
+    public function orcamento(Orcamento $orcamento)
+    {
+        $caminhoArquivo = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'orcamento.zip';
+        $zip = new \ZipArchive();
+        $zip->open($caminhoArquivo,\ZipArchive::CREATE);
+        $zip->addFromString('orcamento.serial', serialize($orcamento));
+
+        $zip->close();
+    }
+}
